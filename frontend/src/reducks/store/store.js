d575a33284018d1254c82bd0aa5fcbf09e06fca3
Estabrok <59147362+Estabrok@users.noreply.github.com>
2022-05-31 18:24:18 -0400

    updated frontend

--- frontend/src/reducks/store/store.js
+++ frontend/src/reducks/store/store.js
@@ -1,18 +1,19 @@
 import { createStore as reduxCreateStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
+import { MoviesReducer } from '../movies/reducers';
+import { FavourotesReducer } from '../favourites/reducers';
 
-import { PostsReducer } from '../posts/reducers';
 
 export default function createStore(history) {
     return reduxCreateStore(
         combineReducers({
             router: connectRouter(history),
-            posts: PostsReducer
+            movies: MoviesReducer,
+            favourites: FavourotesReducer
         }),
         compose(
-            applyMiddleware(routerMiddleware(history), thunk)
-            // DEBUG MODE
+            applyMiddleware(routerMiddleware(history), thunk),
             // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     );
